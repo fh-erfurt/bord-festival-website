34d27d9dc3a8605f5667af54595bc087b01f66fe
Daniel <Daniel@DESKTOP-KJBOI1M>
2019-11-26 02:21:24 +0100

    countdown für Startseite hinzugefügt

---
+++ assets/js/countdown.js
@@ -0,0 +1,50 @@
+var countDownDate = new Date("Jul 31, 2020 10:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  var preseconds = "";
+  var preminutes = "";
+  var prehours = "";
+
+  if(seconds >= 0 && seconds <= 9) {
+    preseconds = "0";
+  } else {
+    preseconds = "";
+  }
+
+  if(minutes >= 0 && minutes <= 9) {
+    preminutes = "0";
+  } else {
+    preminutes = "";
+  }
+
+  if(hours >= 0 && hours <= 9) {
+    prehours = "0";
+  } else {
+    prehours = "";
+  }
+  console.log(prehours);
+
+  // Display the result in the element with id="countdown"
+  document.getElementById("countdown-days").innerHTML = days + " Tage";
+  document.getElementById("countdown-time").innerHTML = prehours + hours + ":" + preminutes + minutes + ":" + preseconds + seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
--- views/pages/index.php
+++ views/pages/index.php
@@ -1,3 +1,10 @@
+<script type="text/javascript" src="assets/js/countdown.js"></script>
+<div class="center countdown">
+    <p id="countdown-days"></p>
+    <p id="countdown-time"></p>
+</div>
+
+<br/>
 Index !!!
 
 <?=$myValue?>
