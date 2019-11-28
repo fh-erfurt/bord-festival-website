16199b179d80b5bd81a6bdfb603cb8d68eddc437
Daniel <Daniel@DESKTOP-KJBOI1M>
2019-11-28 01:28:24 +0100

    database: added table 'adressen'

--- src/database/festival.sql
+++ src/database/festival.sql
@@ -1,67 +1,49 @@
-CREATE DATABASE festival DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- Erstellungsskript für BORD-Festival-PHP
+-- Github-Repo: https://github.com/fh-erfurt/bord-festival-website
+-- erstellt am 27.11.2019
+-- von Raphael Freybe, Daniel Depta
+
+-- Erstellen der Datenbank festival
+
+DROP DATABASE IF EXISTS festival;
+CREATE DATABASE IF NOT EXISTS festival DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+-- Wechseln in den Kontext festival
 
 USE festival;
 
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Erstellungszeit: 28. Nov 2019 um 00:16
--- Server-Version: 10.1.40-MariaDB
--- PHP-Version: 7.1.29
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Datenbank: `festival`
---
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `client`
+-- Tabellenstruktur für Tabelle adressen
 --
+DROP TABLE IF EXISTS adressen;
 
-CREATE TABLE `client` (
-  `ID` int(11) NOT NULL,
-  `MAIL` varchar(100) NOT NULL,
-  `VORNAME` varchar(50) NOT NULL,
-  `NACHNAME` varchar(50) NOT NULL,
-  `PASSWORD` binary(60) NOT NULL,
-  `SALT` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indizes der exportierten Tabellen
---
+CREATE TABLE IF NOT EXISTS adressen
+	(
+		ADRESSID int(7) NOT NULL AUTO_INCREMENT,
+		STRASSE_HNR varchar(100) NOT NULL,
+		PLZ varchar(10) NOT NULL,
+		ORT varchar(100) NOT NULL,
+		LAND varchar(30) default 'GER',
+		CONSTRAINT adressen_pk PRIMARY KEY (ADRESSID)
+	);
 
 --
--- Indizes für die Tabelle `client`
---
-ALTER TABLE `client`
-  ADD PRIMARY KEY (`ID`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
+-- Tabellenstruktur für Tabelle clients
 --
 
---
--- AUTO_INCREMENT für Tabelle `client`
---
-ALTER TABLE `client`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE clients (
+  CLIENTID int(7) NOT NULL AUTO_INCREMENT,
+  MAIL varchar(100) NOT NULL,
+  VORNAME varchar(50) NOT NULL,
+  NACHNAME varchar(50) NOT NULL,
+  GEBURTSDATUM date NOT NULL,
+  PASSWORD binary(60) NOT NULL,
+  SALT varchar(100) NOT NULL,
+  ADRESSID int (7) NOT NULL,
+  CONSTRAINT clients_pk PRIMARY KEY (CLIENTID),
+  CONSTRAINT mail_uq UNIQUE (MAIL),
+  CONSTRAINT clients_adressen_fk FOREIGN KEY (ADRESSID) REFERENCES ADRESSEN (ADRESSID)
+);
