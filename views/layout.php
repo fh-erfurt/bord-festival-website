c50d2678ddea658ee2409ba7c5ea7bc3f7dfb941
ddepta <deventsx64@gmail.com>
2020-01-03 15:23:07 +0100

    highlighting current page in menu

--- views/layout.php
+++ views/layout.php
@@ -1,3 +1,11 @@
+<?php
+$activeAction = '';
+if(isset($_GET['a']))
+{
+	$activeAction = $_GET['a'];
+}
+?>
+
 <html>
 	<head>
 		<title><?php echo $title ?? "BORD-Festival" ?></title>
@@ -27,21 +35,21 @@
 									<p class="festival-logo"><a class="nav-header" href="index.php">BORD-Festival</a></p>
 								</div>
 								<div class="col-lg-8 col-md-12 col-sm-12 float-left nav-menu">
-									<a class="nav-link" href="?a=ticketshop">Tickets</a>
-									<a class="nav-link" href="?a=contact">Kontakt</a>
+									<a class="nav-link<?php echo ($activeAction === 'ticketshop' ? ' nav-link-active' : ''); ?>" href="?a=ticketshop">Tickets</a>
+									<a class="nav-link<?php echo ($activeAction === 'contact' ? ' nav-link-active' : ''); ?>" href="?a=contact">Kontakt</a>
 									<?php if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true) : ?>
-									<a class="nav-link" href="?a=profile">Account</a>
+									<a class="nav-link<?php echo ($activeAction === 'profile' ? ' nav-link-active' : ''); ?>" href="?a=profile">Account</a>
 									<a class="nav-link" href="?a=logout">Abmelden</a>
 									<?php else : ?>
-									<a class="nav-link" href="?a=login">Login</a>
-									<a class="nav-link" href="?a=register">Registrieren</a>
+									<a class="nav-link<?php echo ($activeAction === 'login' ? ' nav-link-active' : ''); ?>" href="?a=login">Login</a>
+									<a class="nav-link<?php echo ($activeAction === 'register' ? ' nav-link-active' : ''); ?>" href="?a=register">Registrieren</a>
 									<?php endif; ?>
 									
 									<?php if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true && isset($_SESSION['client_id'])) : ?>
 										<?php if(empty($carttotalcount) || empty($carttotalprice)) : ?>
-											<a class="nav-link" href="?a=shoppingcart">Warenkorb (<?php echo $carttotalcount ?>)</a>
+											<a class="nav-link<?php echo ($activeAction === 'shoppingcart' ? ' nav-link-active' : ''); ?>" href="?a=shoppingcart">Warenkorb (<?php echo $carttotalcount ?>)</a>
 										<?php else : ?>							
-											<a class="nav-link" href="?a=shoppingcart">Warenkorb (<?php echo $carttotalcount ?>): <?php echo $carttotalprice ?> €</a>
+											<a class="nav-link<?php echo ($activeAction === 'shoppingcart' ? ' nav-link-active' : ''); ?>" href="?a=shoppingcart">Warenkorb (<?php echo $carttotalcount ?>): <?php echo $carttotalprice ?> €</a>
 										<?php endif; ?>
 									<?php endif; ?>
 								</div>
