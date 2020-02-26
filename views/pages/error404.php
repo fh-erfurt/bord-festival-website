bacdb1c3e6b0b8e1ef3d74d017944ad3df844744
rfreybe <r.freybe@salonluchs.de>
2020-02-26 17:42:52 +0100

    adjusted error404

--- views/pages/error404.php
+++ views/pages/error404.php
@@ -1,10 +1,12 @@
 <div class="row">
 	<div class="col-lg-6 col-md-10 col-sm-10 center clear-fix">
 		<section>
-			<h1>Error 404 - Not Found</h1>
-			<p>
-				Seite konnte nicht gefunden werden!
-			</p>
+			<div class="alert alert-danger">
+				<h1>Error 404 - Not Found</h1>
+				<p>
+					Seite konnte nicht gefunden werden!
+				</p>
+			</div>
 		</section>
 	</div>
 </div>
